24b6269d4e7c5e6550267f1494b8d314dd04d7c2
aminta5 <fizi2u@yahoo.com>
2019-01-16 13:51:07 +0100

    Initial Project Add

---
+++ src/main/java/com/agartha/filippetclinic/FilipPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.agartha.filippetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilipPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilipPetClinicApplication.class, args);
+    }
+
+}
+
