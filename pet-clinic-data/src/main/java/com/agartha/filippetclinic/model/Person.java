034208b69db131a671f78a2b0cb8e8c97a94ba73
aminta5 <fizi2u@yahoo.com>
2019-01-16 18:01:38 +0100

    adding base entity closes #10

--- pet-clinic-data/src/main/java/com/agartha/filippetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/agartha/filippetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.agartha.filippetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/agartha/filippetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/agartha/filippetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.agartha.filippetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
