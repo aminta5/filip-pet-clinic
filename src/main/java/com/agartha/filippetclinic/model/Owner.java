22d67bc8acd41af15caa49b07c2a46ce36733c3e
aminta5 <fizi2u@yahoo.com>
2019-01-16 14:34:51 +0100

    adding POJOS. Closes #2.

---
+++ src/main/java/com/agartha/filippetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.agartha.filippetclinic.model;
+
+public class Owner extends Person{
+    
+}
---
+++ src/main/java/com/agartha/filippetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.agartha.filippetclinic.model;
+
+public class Vet extends Person{
+
+}
