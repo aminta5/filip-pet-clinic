04c6a185905d9815ba3ae6fe4d6aeb613ea10bb4
aminta5 <fizi2u@yahoo.com>
2019-01-17 21:46:04 +0100

    implement Spring conf services closes #22

--- pet-clinic-web/src/main/java/com/agartha/filippetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/agartha/filippetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.agartha.filippetclinic.model.Owner;
 import com.agartha.filippetclinic.model.Vet;
 import com.agartha.filippetclinic.services.OwnerService;
 import com.agartha.filippetclinic.services.VetService;
-import com.agartha.filippetclinic.services.map.OwnerServiceMap;
-import com.agartha.filippetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -16,9 +14,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
