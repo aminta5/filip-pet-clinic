c146ca7b890f8a4ce73552af0572ba2b9e9c5734
aminta5 <fizi2u@yahoo.com>
2019-01-18 22:33:09 +0100

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/com/agartha/filippetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.agartha.filippetclinic.services;
+
+import com.agartha.filippetclinic.model.PetType;
+
+public interface PetTypeService extends MyCrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/agartha/filippetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package com.agartha.filippetclinic.services.map;
+
+import com.agartha.filippetclinic.model.PetType;
+import com.agartha.filippetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType var) {
+        return super.save(var);
+    }
+
+    @Override
+    public void delete(PetType type) {
+        super.deleteByObject(type);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
