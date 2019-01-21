0c59d1728ff1611fa6f49d56c62c14710a3eddd6
aminta5 <fizi2u@yahoo.com>
2019-01-21 12:54:56 +0100

    added spring data JPA services for Vet Specialities  . Closes #44

---
+++ pet-clinic-data/src/main/java/com/agartha/filippetclinic/services/springdatajpa/VetSpecialityJpaService.java
@@ -0,0 +1,49 @@
+package com.agartha.filippetclinic.services.springdatajpa;
+
+import com.agartha.filippetclinic.model.Speciality;
+import com.agartha.filippetclinic.repositories.SpecialityRepository;
+import com.agartha.filippetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSpecialityJpaService implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public VetSpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities :: add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality var) {
+        return specialityRepository.save(var);
+    }
+
+    @Override
+    public void delete(Speciality type) {
+        specialityRepository.delete(type);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+
+    }
+}
